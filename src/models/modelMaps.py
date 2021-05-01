20c06a132bbdbffd1ddd79382fd28b282206f3d4
Ioannis Athanasiadis <sniper@localhost.localdomain>
2021-05-01 14:33:08 +0200

    fix: test and fix the tokenizers and models. Also finding out which models work for this scenario

--- src/models/modelMaps.py
+++ src/models/modelMaps.py
@@ -1,28 +1,34 @@
 import transformers
+import tensorflow as tf
 import typing
+
+def getGPT2Model() ->transformers.TFGPT2ForSequenceClassification:
+    model = transformers.TFGPT2ForSequenceClassification.from_pretrained("gpt2")
+    model.training = True
+    return model
 
 def getModelMap() -> typing.Dict[str, typing.Callable]:
     return {
-        "roberta": lambda: transformers.TFRobertaForSequenceClassification.from_pretrained('roberta-base'),
-        "electra": lambda: transformers.TFElectraForSequenceClassification.from_pretrained('google/electra-small-discriminator'),
-        "albert" : lambda: transformers.TFAlbertForSequenceClassification.from_pretrained('albert-base-v2'),
-        "bart": lambda: transformers.BartForSequenceClassification.from_pretrained('facebook/bart-large'),
-        "berttweet": lambda: transformers.AutoModel.from_pretrained("vinai/bertweet-base"),
-        # "bigbird": lambda: transformers.BigBirdForSequenceClassification.from_pretrained('google/bigbird-roberta-base'),
-        "convbert": lambda: transformers.TFConvBertForSequenceClassification.from_pretrained('bert-base-uncased'),
-        "ctrl": lambda: transformers.TFCTRLForSequenceClassification.from_pretrained('ctrl'),
-        "deberta": lambda: transformers.DebertaForSequenceClassification.from_pretrained('microsoft/deberta-base'),
-        "debertav2": lambda: transformers.DebertaV2ForSequenceClassification.from_pretrained('microsoft/deberta-v2-xlarge'),
-        "distilbert": lambda: transformers.TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased'),
-        "funnel": lambda: transformers.TFFunnelForSequenceClassification.from_pretrained('funnel-transformer/small-base'),
-        # "ibert": lambda: transformers.IBertForSequenceClassification.from_pretrained('kssteven/ibert-roberta-base'),
-        "mpnet": lambda: transformers.TFMPNetForSequenceClassification.from_pretrained('microsoft/mpnet-base'),
-        # "gpt": lambda: transformers.TFOpenAIGPTForSequenceClassification.from_pretrained('openai-gpt'), # type object 'EnglishDefaults' has no attribute 'create_tokenizer'
-        "gpt2": lambda: transformers.TFGPT2ForSequenceClassification.from_pretrained("gpt2"),
-        "squeezebert": lambda: transformers.SqueezeBertForSequenceClassification.from_pretrained('squeezebert/squeezebert-uncased'),
-        "transformerxl": lambda: transformers.TransfoXLForSequenceClassification.from_pretrained('transfo-xl-wt103'),
-        "xlm-mlm": lambda: transformers.TFXLMForSequenceClassification.from_pretrained('xlm-mlm-en-2048'),
-        "xlnet": lambda: transformers.TFXLNetForSequenceClassification.from_pretrained('xlnet-large-cased')
+        "roberta": lambda: transformers.TFRobertaForSequenceClassification.from_pretrained('roberta-base'), # OK
+        "electra": lambda: transformers.TFElectraForSequenceClassification.from_pretrained('google/electra-small-discriminator'), # OK
+        "albert" : lambda: transformers.TFAlbertForSequenceClassification.from_pretrained('albert-base-v2'), # OK
+        "bart": lambda: transformers.BartForSequenceClassification.from_pretrained('facebook/bart-large'), # OK
+        # "berttweet": lambda: transformers.AutoModel.from_pretrained("vinai/bertweet-base"), # accepts only inference (no retraining)
+        # "bigbird": lambda: transformers.BigBirdForSequenceClassification.from_pretrained('google/bigbird-roberta-base'), # NOT OK (I dont remember why)
+        "convbert": lambda: transformers.TFConvBertForSequenceClassification.from_pretrained('bert-base-uncased'), # OK
+        "ctrl": lambda: transformers.TFCTRLForSequenceClassification.from_pretrained('ctrl'), # OK (needs too much memory)
+        "deberta": lambda: transformers.DebertaForSequenceClassification.from_pretrained('microsoft/deberta-base'), # OK
+        "debertav2": lambda: transformers.DebertaV2ForSequenceClassification.from_pretrained('microsoft/deberta-v2-xlarge'), # OK
+        "distilbert": lambda: transformers.TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased'), # OK
+        "funnel": lambda: transformers.TFFunnelForSequenceClassification.from_pretrained('funnel-transformer/small-base'), # OK
+        # "ibert": lambda: transformers.IBertForSequenceClassification.from_pretrained('kssteven/ibert-roberta-base'), # NOT OK
+        "mpnet": lambda: transformers.TFMPNetForSequenceClassification.from_pretrained('microsoft/mpnet-base'), # OK
+        "gpt": lambda: transformers.TFOpenAIGPTForSequenceClassification.from_pretrained('openai-gpt'), # type object 'EnglishDefaults' has no attribute 'create_tokenizer'
+        "gpt2": getGPT2Model, # OK (WIP trains only with batch size = 64 and seems it trains very fast)
+        "squeezebert": lambda: transformers.SqueezeBertForSequenceClassification.from_pretrained('squeezebert/squeezebert-uncased'), # OK
+        "transformerxl": lambda: transformers.TransfoXLForSequenceClassification.from_pretrained('transfo-xl-wt103'), # OK (demands insane amount of memory)
+        "xlm-mlm": lambda: transformers.TFXLMForSequenceClassification.from_pretrained('xlm-mlm-en-2048'), # OK
+        "xlnet": lambda: transformers.TFXLNetForSequenceClassification.from_pretrained('xlnet-large-cased') # OK
     }
 
 def mapStrToTransformerModel(modelName:str) -> transformers.PreTrainedModel:
--- src/preprocessing/pipelineMaps.py
+++ src/preprocessing/pipelineMaps.py
@@ -1,16 +1,31 @@
 import transformers
 import typing
 
+def getCTRLTokenizer() -> transformers.CTRLTokenizer:
+    tokenizer = transformers.CTRLTokenizer.from_pretrained('ctrl')
+    tokenizer.pad_token = ' '
+    return tokenizer
+
+def getGPT2Tokenizer() -> transformers.GPT2Tokenizer:
+    tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
+    tokenizer.pad_token = ' '
+    return tokenizer
+
+def getTransfoXLTokenizer() -> transformers.TransfoXLTokenizer:
+    tokenizer = transformers.TransfoXLTokenizer.from_pretrained('transfo-xl-wt103')
+    tokenizer.pad_token = ' '
+    return tokenizer
+    
 def getTokenizerMap() -> typing.Dict[str, typing.Callable]:
     return {
-        "roberta": lambda: transformers.TFRobertaForSequenceClassification.from_pretrained('roberta-base'),
+        "roberta": lambda: transformers.RobertaTokenizer.from_pretrained('roberta-base'),
         "electra": lambda: transformers.ElectraTokenizer.from_pretrained('google/electra-small-discriminator'),
         "albert" : lambda: transformers.AlbertTokenizer.from_pretrained('albert-base-v2'),
         "bart" : lambda: transformers.BartTokenizer.from_pretrained('facebook/bart-large'),
-        "berttweet" : lambda: transformers.BertweetTokenizer.from_pretrained('vinai/bertweet-base', use_fast=False),
+        "berttweet" : lambda: transformers.BertweetTokenizer.from_pretrained('vinai/bertweet-base', use_fast=False), # accepts only inference (no retraining)
         # "bigbird" : lambda: transformers.BigBirdTokenizer.from_pretrained('google/bigbird-roberta-base'),
         "convbert": lambda: transformers.ConvBertTokenizer.from_pretrained('bert-base-uncased'),
-        "ctrl": lambda: transformers.CTRLTokenizer.from_pretrained('ctrl'),
+        "ctrl": getCTRLTokenizer,
         "deberta": lambda: transformers.DebertaTokenizer.from_pretrained('microsoft/deberta-base'),
         "debertav2": lambda: transformers.DebertaV2Tokenizer.from_pretrained('microsoft/deberta-v2-xlarge'),
         "distilbert": lambda: transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased'),
@@ -18,9 +33,9 @@ def getTokenizerMap() -> typing.Dict[str, typing.Callable]:
         # "ibert": lambda: transformers.RobertaTokenizer.from_pretrained('kssteven/ibert-roberta-base'),
         "mpnet": lambda: transformers.MPNetTokenizer.from_pretrained('microsoft/mpnet-base'),
         # "gpt": lambda: transformers.OpenAIGPTTokenizer.from_pretrained('openai-gpt'), # type object 'EnglishDefaults' has no attribute 'create_tokenizer'
-        "gpt2": lambda: transformers.GPT2Tokenizer.from_pretrained("gpt2"),
+        "gpt2": getGPT2Tokenizer,
         "squeezebert": lambda: transformers.SqueezeBertTokenizer.from_pretrained('squeezebert/squeezebert-uncased'),
-        "transformerxl": lambda: transformers.TransfoXLTokenizer.from_pretrained('transfo-xl-wt103'),
+        "transformerxl": getTransfoXLTokenizer,
         "xlm-mlm": lambda: transformers.XLMTokenizer.from_pretrained('xlm-mlm-en-2048'),
         "xlnet": lambda: transformers.XLNetTokenizer.from_pretrained('xlnet-large-cased')
     }
