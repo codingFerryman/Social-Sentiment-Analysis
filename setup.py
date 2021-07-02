d926b0689d5977e23d02918b99527a409a5f2ea2
He Liu <9249032+codingFerryman@users.noreply.github.com>
2021-07-02 11:40:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
+import os
+_current_path = os.path.dirname(os.path.realpath(__file__))
+_src_path = os.path.join(_current_path, "src")
 
 setup(
     name='CIL',
     version='1.0',
-    packages=find_packages(where="src"),
-    package_dir={'': 'src'},
+    packages=find_packages(where=_src_path),
+    package_dir={'': _src_path},
     url='https://github.com/supernlogn/Computational-Intelligence-Lab',
     license='',
     author='',
