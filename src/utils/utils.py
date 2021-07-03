031f20f9e1f705054ac8cf83989372ab0173ff6e
He Liu <heliu@heliu.name>
2021-07-03 14:12:32 +0100

    Fix utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -27,7 +27,7 @@ def get_transformers_layers_num(model: PreTrainedModel) -> int:
 def prepend_multiple_lines(file_name, list_of_lines):
     """Insert given list of strings as new lines at the beginning of a file"""
     # define name of temporary dummy file
-    dummy_file = file_name + '.bak'
+    dummy_file = str(file_name) + '.bak'
     # open given original file in read mode and dummy file in write mode
     with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
         # Iterate over the given list of strings and write them to dummy file as lines
