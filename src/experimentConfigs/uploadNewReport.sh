0261dba13450483e2ebb20b609c6bd926f3f57e6
supernlogn <ath.ioannis94@gmail.com>
2021-07-06 12:11:01 +0300

    doc: new report after running experiments

--- src/experimentConfigs/uploadNewReport.sh
+++ src/experimentConfigs/uploadNewReport.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-gif diff report.json # output diff
-# upload the new report
-git add report.json
-git commit -m "doc: new report after running experiments"
-git push -u origin master
+if [[ ! -z $1 ]]; then
+    REPORT_FILE=$1
+else
+    REPORT_FILE="../../docs/report.json"
+fi
+DIFF=`git diff $REPORT_FILE`
+echo $DIFF # output diff
+if [[ ! -z $DIFF ]]; then
+    # upload the new report
+    git add $REPORT_FILE
+    git commit -m "doc: new report after running experiments"
+    git push -u origin master
+fi
