ccc4423c3b9efc7169a818a599a4ada7cfc4a6be
He Liu <heliu@heliu.name>
2021-07-06 09:39:07 +0100

    Update requirements

--- setup_environment.sh
+++ setup_environment.sh
@@ -11,10 +11,12 @@ python -m venv "${SCRIPT_DIR}"/venv
 source "${SCRIPT_DIR}"/venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
-pip install torch==1.8.1+cu102 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
-pip install 'torchtext<0.10'
-pip install --upgrade 'spacy[cuda102]'
+
+# CUDA
+pip install -r "${SCRIPT_DIR}"/requirements.txt
+# CPU
+#pip install -r "${SCRIPT_DIR}"/requirements_cpu.txt
+
 spacy download en_core_web_sm
 #spacy download en_core_web_trf
-pip install -r "${SCRIPT_DIR}"/requirements.txt
 #python "${SCRIPT_DIR}"/setup.py install
