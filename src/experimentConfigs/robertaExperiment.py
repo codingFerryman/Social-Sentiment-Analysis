18404a3f9763c5fc7920e53df43d852e362e042b
He Liu <heliu@heliu.name>
2021-07-07 09:38:48 +0100

    Adapt the example of robertaexperiment to new configuration and pipelines

--- src/experimentConfigs/robertaExperiment.py
+++ src/experimentConfigs/robertaExperiment.py
@@ -4,25 +4,36 @@ from pathlib import Path
 
 from models import TransformersModel
 # sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from preprocessing import PretrainedTransformersPipeLine
+from submission import TransformersPredict
 from utils import get_project_path
+import torch
 
 project_directory = get_project_path()
 
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"
-# Load the model
-# model_name = 'cardiffnlp/twitter-roberta-base-sentiment'
-model_name = 'roberta-base'
-pipeline = PretrainedTransformersPipeLine(model_name)
-pipeline.loadData(ratio=0.001)
-# encDataTrain, encDataVal = pipeline.getEncodedDataset(splitter=train_test_split, test_size=0.1)
-model = TransformersModel(modelName_or_pipeLine=pipeline)
+if torch.cuda.is_available():
+    cuda_rank = 0
+else:
+    cuda_rank = -1
+os.environ['CUDA_VISIBLE_DEVICES'] = str(cuda_rank)
 
-with open(Path(project_directory, 'src', 'experimentConfigs', './robertaDefault.json'), 'r') as fp:
+# Load the config
+with open(Path(project_directory, 'src', 'configs', 'roberta_base_debug.json'), 'r') as fp:
     config = json.load(fp)
 
-metric = ['glue', 'mrpc']
-model.registerMetric(*metric)
+# Load the model
+model_name_or_path = config['model_name_or_path']
+model = TransformersModel(modelName_or_pipeLine=model_name_or_path,
+                          fast_tokenizer=config.get('fast_tokenizer'))
+
+# Register metrics
+if type(config['metric']) is str:
+    config['metric'] = [config['metric']]
+model.registerMetric(*config['metric'])
+
+# Load training data
+model.loadData(ratio=d['data_load_ratio'])
+
+# Train the model and get evaluation history
 eval_log = model.trainModel(
     train_val_split_iterator=config['args'].get('train_val_split_iterator', "train_test_split"),
     model_config=config['model_config'],
@@ -30,30 +41,22 @@ eval_log = model.trainModel(
     trainer_config=config['args']
 )
 
-trainer = model.getTrainer()
-# trainer.save_model(PurePath(project_directory, 'trainings', 'model', model_name, time.strftime("%Y%m%d-%H%M%S")))
+# Save the model
+model.save()
 
-# %%
-#
-# # Make predictions
-# test_loader = DataLoader(test_text, batch_size=64)
-# predictions = torch.tensor([], device=device)
-# with torch.no_grad():
-#     for test_data in test_loader:
-#         inputs = tokenizer.batch_encode_plus(truncation=True, padding=True, max_length=max_test)
-#         inputs = inputs.to(device)
-#         logit = model(**inputs).logits
-#         prediction = torch.argmax(torch.softmax(logit,dim=-1), dim=-1)
-#         predictions = torch.cat((predictions, prediction), 0)
-#
-# #%%
-#
-# # Make the predictions be compatible with the submission
-# pred = predictions.int().tolist()
-# # pred = np.where(pred==0, -1, pred)
-# pred_id = test_id+zero_len_idx_test
-# pred_est = pred+[random.choice([0,1]) for i in range(len(zero_len_idx_test))]
-# pred_est = [p if p==1 else -1 for p in pred_est]
-# pred_dict = {'Id': pred_id, 'Prediction': pred_est}
-# pred_df = pd.DataFrame(pred_dict)
-# pred_df.to_csv('./submission.csv', index=False)
+# Get the best result
+best_model_metric = model.getBestMetric()
+
+# Get the path of the model
+model_saved_path = model.training_saving_path
+
+# Load back the trainer
+trainer = model.getTrainer()
+
+# Predict the test data
+model_predict = TransformersPredict(load_path=model_saved_path,
+                                    text_path=Path(project_directory, 'data', 'test_data.txt'),
+                                    cuda_device=cuda_rank)
+model_predict.predict(batch_size=32)
+
+model_predict.submission_file(Path(model_saved_path, 'submission.csv'))
