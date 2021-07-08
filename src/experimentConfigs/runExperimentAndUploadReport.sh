03d7d8bb0553244f401fefbb8d29f92e1788afc1
supernlogn <ath.ioannis94@gmail.com>
2021-07-08 03:01:52 +0300

    load modules in cluster

--- src/experimentConfigs/runExperimentAndUploadReport.sh
+++ src/experimentConfigs/runExperimentAndUploadReport.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 declare TEST_PATH=$1
-
+module load gcc/6.3.0 python_gpu/3.8.5 hdf5/1.10.1 eth_proxy
+source cil-venv/bin/activate
 python experiment.py test_path=$TEST_PATH report_path='../../docs/report.json'
 sh uploadNewReport.sh
