8382dd615fa32dd9eebd88b33d8e07726f9c9a57
He Liu <heliu@heliu.name>
2021-07-11 10:29:22 +0100

    docs: predict the train and test data fix: fast_tokenizer=false not working

--- src/experimentConfigs/submission.py
+++ src/experimentConfigs/submission.py
@@ -37,7 +37,7 @@ class TransformersPredict:
         try:
             tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=fast_tokenizer)
         except Exception as e:
-            raise (e, "Please try to disable fast tokenizer by \'fast_tokenizer=False\'and try again")
+            raise (e, "Please try to disable fast tokenizer by \'fast_tokenizer=false\'and try again")
 
         self.pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer, device=cuda_device,
                                                    binary_output=True)
@@ -70,11 +70,13 @@ class TransformersPredict:
         pred_df.to_csv(save_path, index=False)
 
     def pre_process_test(self, lines: list):
-        # Cleaning test set
         if self.is_test:
+            # Clean test set
             data = [s.split(',', 1)[-1] for s in lines]
             ids = [s.split(',', 1)[0] for s in lines]
+
         else:
+            # Clean the train set
             data = []
             ids = []
             for s in tqdm(lines):
@@ -99,16 +101,31 @@ class TransformersPredict:
 
 
 def main(args: list):
+    """
+    The main function of submission. It will predict the sentiment of texts in the text data
+    Args:
+        args (list): a dictionary containing the program arguments (sys.argv)
+        - load_path: The root directory containing 'model' and 'tokenizer'
+        - batch_size: The batch size in prediction
+        - cuda_device: The index of cuda device for prediction.
+            If not given, the program will automatically use the first cuda device otherwise the cpu
+        - fast_tokenizer: Use Fast Tokenizer or not in predictions. Better to use the same as training tokenizer
+            Using normal tokenizer by default
+        - text_path: The text file to be processed. data/test_data.txt by default
+
+    Returns:
+
+    """
     argv = {a.split('=')[0]: a.split('=')[1] for a in args[1:]}
     load_path = argv.get('load_path', None)
-    text_path = argv.get('text_path', None)
+    assert load_path, "No load_path specified"
     batch_size = argv.get('batch_size', 2000)
     cuda_device = argv.get('cuda', None)
-    fast_tokenizer = bool(argv.get('fast_tokenizer', False))
 
-    if load_path is None:
-        print("No load_path specified")
-        exit(0)
+    text_path = argv.get('text_path', None)
+    fast_tokenizer = argv.get('fast_tokenizer', 'false').lower()
+    assert fast_tokenizer in ['true', 'false']
+    fast_tokenizer = False if 'f' in fast_tokenizer else True
 
     if text_path is None:
         data_path = get_data_path()
--- src/explorations/evaluate_trainset.py
+++ src/explorations/evaluate_trainset.py
@@ -17,20 +17,25 @@ data_path = get_data_path()
 
 class TransformersPredictEval(TransformersPredict):
     def __init__(self, load_path, fast_tokenizer, full_or_sub: str, text_path=None, pos_path=None, neg_path=None,
-                 cuda_device=None,
-                 is_test=False):
-        if full_or_sub == 'sub':
-            full_or_sub_file_suffix = ''
-        elif full_or_sub == 'full':
-            full_or_sub_file_suffix = '_full'
-        else:
-            logger.error('full_or_sub should be full or sub')
-            exit(0)
+                 cuda_device=None, is_test=False):
+        # Generate the data file by combine and shuffle pos and neg data if no text_path
         if text_path is None:
+            # Adapt inputs to real filenames
+            assert full_or_sub in ['sub', 'full'], 'full_or_sub should be full or sub'
+            if full_or_sub == 'sub':
+                full_or_sub_file_suffix = ''
+            elif full_or_sub == 'full':
+                full_or_sub_file_suffix = '_full'
+
             if pos_path is None:
                 pos_path = Path(data_path, 'train_pos' + full_or_sub_file_suffix + '.txt')
             if neg_path is None:
                 neg_path = Path(data_path, 'train_neg' + full_or_sub_file_suffix + '.txt')
+            logger.debug('Loading data from:')
+            logger.debug(pos_path)
+            logger.debug(neg_path)
+
+            # Read data files and set the labels
             tmp = []
             with open(pos_path, 'r') as fp:
                 pos_text = list(set(fp.readlines()))
@@ -41,55 +46,80 @@ class TransformersPredictEval(TransformersPredict):
             tmp.extend(pos_data)
             tmp.extend(neg_data)
             random.shuffle(tmp)
-            self.data_indexed = [(i,) + d for i, d in enumerate(tmp)]
-            data2write = ['\u0001'.join(str(d) for d in doc) for doc in self.data_indexed]
+            data_indexed = [(i,) + d for i, d in enumerate(tmp, 1)]
+            # ... then write the data to a file. The separator here is invisible
+            data2write = ['\u0001'.join(str(d) for d in doc) for doc in data_indexed]
             text_path = Path(data_path, full_or_sub + '_data.txt')
             with open(text_path, 'w') as ft:
                 ft.writelines(data2write)
+        # Init superclass
         super(TransformersPredictEval, self).__init__(load_path, text_path, fast_tokenizer, cuda_device, is_test)
 
     def evaluation_file(self, save_path=None):
         if save_path is None:
             save_path = Path(self.load_path, 'prediction_on_train.csv')
+        # The golden data read from the file
+        data_indexed = pd.read_csv(self.text_path, sep='\u0001', names=['golden', 'text'])
+        data_indexed['text'] = data_indexed['text'].str.strip()
+
+        # The prediction labels, scores, and the index of zero length sentences
         pred_labels = [r['label'] for r in self.pred]
         pred_score = [r['score'] for r in self.pred]
         id_zero_len = self.data['zero_len_ids']
+
+        # Randomly predict the label of zero length sentences, and set their probability to 0.5
         pred_id = self.data['ids'] + id_zero_len
         pred_est = pred_labels + [random.choice([-1, 1]) for _ in range(len(id_zero_len))]
         pred_est_score = pred_score + [0.5 for _ in range(len(id_zero_len))]
-        golden_data = [self.data_indexed[int(_idx)] for _idx in pred_id]
-        golden_label = [idg[1] for idg in golden_data]
-        original_text = [idg[2].rstrip() for idg in golden_data]
-        pred_dict = {'Id': pred_id,
-                     'Golden': golden_label,
-                     'Prediction': pred_est,
-                     'Score': pred_est_score,
-                     'Text': original_text}
-        pred_df = pd.DataFrame(pred_dict)
-        pred_df.to_csv(save_path, index=False)
+        # ... and save the predictions to DataFrame
+        pred_df_dict = {'index': pred_id,
+                        'pred': pred_est,
+                        'score': pred_est_score}
+        pred_df = pd.DataFrame.from_dict(pred_df_dict)
+        pred_df.set_index('index', inplace=True)
+
+        # Join the prediction DataFrame and the golden DataFrame
+        results = data_indexed.join(pred_df)
+        # ... and write it to a csv file
+        results.to_csv(save_path, index=False)
 
 
 def main(args: list):
+    """
+    The main function of the evaluation. It will predict the sentiment of texts in the train data
+    Args:
+        args (list): a dictionary containing the program arguments (sys.argv)
+        - load_path: The root directory containing 'model' and 'tokenizer'
+        - batch_size: The batch size in prediction
+        - cuda_device: The index of cuda device for prediction.
+            If not given, the program will automatically use the first cuda device otherwise the cpu
+        - fast_tokenizer: Use Fast Tokenizer or not in predictions. Better to use the same as training tokenizer
+            Using fast tokenizer by default
+        - text_path: The text file to be processed. It will overwrite the full_or_sub
+        - full_or_sub: Use full or sub dataset. It will be ignored if text_path is given
+    Call it like:
+        python explorations/evaluate_trainset.py \
+            load_path=trainings/MODEL_NAME/TIMESTAMP \
+            fast_tokenizer=false full_or_sub=sub
+    """
     argv = {a.split('=')[0]: a.split('=')[1] for a in args[1:]}
     load_path = argv.get('load_path', None)
-    text_path = argv.get('text_path', None)
+    assert load_path, "No load_path specified"
     batch_size = argv.get('batch_size', 256)
     cuda_device = argv.get('cuda', None)
-    fast_tokenizer = bool(argv.get('fast_tokenizer', True))
-    full_or_sub = argv.get('full_or_sub', 'sub')
-
-    if load_path is None:
-        print("No load_path specified")
-        exit(0)
+    fast_tokenizer = argv.get('fast_tokenizer', 'true').lower()
+    assert fast_tokenizer in ['true', 'false']
+    fast_tokenizer = False if 'f' in fast_tokenizer else True
+    text_path = argv.get('text_path', None)
+    full_or_sub = argv.get('full_or_sub', None)
+    assert text_path or full_or_sub, 'text_path or full_or_sub should be given'
 
     if text_path is None:
+        assert full_or_sub in ['full', 'sub'], 'full_or_sub should be full or sub'
         if full_or_sub == 'full':
             _text_path = Path(data_path, 'full_data.txt')
         elif full_or_sub == 'sub':
             _text_path = Path(data_path, 'sub_data.txt')
-        else:
-            logger.error('full_or_sub should be full or sub')
-            exit(0)
         if _text_path.is_file():
             logger.info("Using the data file which already exists.")
             text_path = _text_path
