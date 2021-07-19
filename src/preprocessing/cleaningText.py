96f7636b1163b6b4d4eb23132d9cce0434c0dff7
He Liu <heliu@heliu.name>
2021-07-19 10:40:04 +0100

    dev: spellchecker

--- src/preprocessing/cleaningText.py
+++ src/preprocessing/cleaningText.py
@@ -1,13 +1,23 @@
+import os
+import sys
+from pathlib import Path
 from typing import Dict, Callable, Union
 
 import modin.pandas as mpd
+import neuspell
 import pandas as pd
 import regex
 from cleantext import clean
 from distributed import Client
+from neuspell import BertChecker
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from tqdm import tqdm
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utils import loggers, get_project_path
+
+logger = loggers.getLogger("CleaningText", True)
+PROJECT_PATH = get_project_path()
 # ProgressBar.enable()
 
 EMOTICONS = r"""
@@ -92,9 +102,11 @@ def cleaning_strip(text: Union[str, list]):
         return _result
 
 
-def cleaning_default_dev(text: Union[str, list]):
+def cleaning_default_dev(text: Union[str, list], spell_checker=None):
     dtknzr = TreebankWordDetokenizer()
     if type(text) is str:
+        if spell_checker:
+            text = spell_checker(text)
         text = dtknzr.detokenize(text.split())
         text = cleaning_masks(text)
         text = clean(text,
@@ -123,7 +135,28 @@ def cleaning_default_dev(text: Union[str, list]):
     return text
 
 
-def cleaning_default_dev_mp(text_list):
+def cleaning_default_dev_mp(text_list, check_spell=True):
+    logger.info("This cleaning method may take 10~30 min, please wait ...")
+    if check_spell is True:
+        spell_checker_path = Path(PROJECT_PATH, 'src', 'preprocessing', 'subwordbert-probwordnoise')
+        spell_checker_exists = spell_checker_path.exists()
+        if Path().resolve().parts[1] == 'cluster' and not spell_checker_exists:
+            logger.info("Set the proxy for downloading spell checker")
+            proxy = "http://proxy.ethz.ch:3128"
+            os.environ['http_proxy'] = proxy
+            os.environ['HTTP_PROXY'] = proxy
+            os.environ['https_proxy'] = proxy
+            os.environ['HTTPS_PROXY'] = proxy
+        if not spell_checker_exists:
+            logger.info("Downloading the spell checker ...")
+            neuspell.seq_modeling.downloads.download_pretrained_model(str(spell_checker_path))
+        else:
+            logger.info("The pre-trained spell checker already exists.")
+        checker = BertChecker()
+        checker.from_pretrained(spell_checker_path)
+        logger.info("Correcting misspelling words ...")
+        text_list = checker.correct_strings(text_list)
+    logger.info("Cleaning text by 8 workers")
     client = Client(n_workers=8)
     _tmp = mpd.Series(text_list)
     _tmp = _tmp.map(cleaning_default_dev)
@@ -143,5 +176,5 @@ def cleaningMap() -> Dict[str, Callable]:
 if __name__ == '__main__':
     with open('../../data/full_data.txt') as fp:
         data = fp.readlines()
-    data = data[:10000]
+    data = data[:100]
     t = cleaning_default_dev_mp(data)
