ab6dde94527d518d336502beff5d538e0b27eac7
He Liu <heliu@heliu.name>
2021-07-19 17:58:25 +0100

    fix: circular import

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,2 +1,3 @@
-from .cleaningText import *
+from .cleaningText import cleaningMap
+from .cleaningText import cleaning_default, cleaning_masks, cleaning_strip, no_cleaning, cleaning_tweet
 from .pretrainedTransformersPipeline import PretrainedTransformersPipeLine
