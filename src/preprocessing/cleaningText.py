4238965723f6cdfe35dddae178bea94bf4fc2992
He Liu <9249032+codingFerryman@users.noreply.github.com>
2021-07-21 13:35:36 +0100

    Fix 'tweet' cleaner for test data

--- src/preprocessing/cleaningText.py
+++ src/preprocessing/cleaningText.py
@@ -131,6 +131,8 @@ def _cleaning_tweet(text: str, spell_checker=None):
 
 
 def cleaning_tweet(text_list, check_spell=False, batch_size=512, is_test=False):
+    if type(text_list) is str:
+        is_test = True
     if check_spell is True:
         spell_checker_path = Path(PROJECT_PATH, 'src', 'preprocessing', 'subwordbert-probwordnoise')
         spell_checker_exists = spell_checker_path.exists()
