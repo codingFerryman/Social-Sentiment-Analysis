196e84f32b6604310341e976c2ca01082ed478e4
He Liu <heliu@heliu.name>
2021-07-26 00:32:01 +0200

    add Lemmatization in data augmentation

--- src/preprocessing/dataAugmentation.py
+++ src/preprocessing/dataAugmentation.py
@@ -2,20 +2,45 @@ import os
 import sys
 from pathlib import Path
 
+from tqdm.auto import tqdm
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.others import get_data_path
 
 DATA_PATH = get_data_path()
 
+if Path().resolve().parts[1] == 'cluster':
+    import stanza
+
+    os.environ['STANZA_RESOURCES_DIR'] = os.path.join(os.getenv("SCRATCH"), '.cache/stanza_resources/')
+    stanza.download(lang='en',
+                    model_dir=os.getenv('STANZA_RESOURCES_DIR'))
+else:
+    import stanza
+
+    stanza.download('en')
+nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,lemma')
+
+
+def lemmatization(text):
+    doc = nlp(text)
+    _result = " ".join([word.lemma for sent in doc.sentences for word in sent.words])
+    return _result
+
 
 def combine_data(*data_path):
     _tmp = []
+    with open(data_path[0], 'r') as fp:
+        _data = fp.readlines()
+        _data_processed = []
+        for _t in tqdm(_data, dynamic_ncols=True, desc="Lemmatization", mininterval=30, maxinterval=60):
+            _data_processed.append(lemmatization(_t) + '\n')
+        _tmp.append(_data_processed)
     for file_path in data_path:
         with open(file_path, 'r') as fp:
             _tmp.append(fp.readlines())
     _result = list(set().union(*_tmp))
     return _result
-
 
 if __name__ == '__main__':
     combine_path_list = [
