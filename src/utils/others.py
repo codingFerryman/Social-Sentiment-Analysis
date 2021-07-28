e5a4a78978a20967b38459d3a58c41baa42f7422
He Liu <heliu@heliu.name>
2021-07-28 14:04:23 +0100

    set_seed for reproduce the results

--- src/utils/others.py
+++ src/utils/others.py
@@ -1,8 +1,24 @@
 import os
+import random
 import re
 from pathlib import Path
 
+import numpy as np
+import torch
 from transformers import PreTrainedModel
+
+
+def set_seed(seed: int):
+    """
+    Helper function for reproducible behavior to set the seed in ``random``, ``numpy``, ``torch``
+
+    Args:
+        seed (:obj:`int`): The seed to set.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 
 def get_project_path() -> Path:
@@ -12,6 +28,7 @@ def get_project_path() -> Path:
         return git.Repo(Path(), search_parent_directories=True).git.rev_parse("--show-toplevel")
     except NameError:
         return Path(__file__).parent.parent
+
 
 def get_data_path() -> Path:
     return Path(get_project_path(), 'data')
