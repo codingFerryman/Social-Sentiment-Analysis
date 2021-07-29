1ff70ce6c2446b1e2f2fa91b2c33290caff2e2ad
He Liu <heliu@heliu.name>
2021-07-29 01:04:45 +0100

    preprocess and clean conflict in the training data

--- src/utils/inputFunctions.py
+++ src/utils/inputFunctions.py
@@ -3,6 +3,7 @@ from pathlib import PurePath
 from typing import Union, Tuple
 
 import numpy as np
+import pandas as pd
 
 from .loggers import getLogger
 from .others import get_data_path
@@ -26,9 +27,9 @@ def loadData(dataDirectory: str = None, ratio: Union[str, float, int] = "full") 
         dataDirectory = DATA_DIRECTORY
 
     with open(PurePath(dataDirectory, 'train_pos_full.txt'), 'r', encoding='utf-8') as fp:
-        train_pos_full = list(set(fp.readlines()))
+        train_pos_full = fp.readlines()
     with open(PurePath(dataDirectory, 'train_neg_full.txt'), 'r', encoding='utf-8') as fp:
-        train_neg_full = list(set(fp.readlines()))
+        train_neg_full = fp.readlines()
     with open(PurePath(dataDirectory, 'test_data.txt'), 'r', encoding='utf-8') as fp:
         test_full = fp.readlines()
 
@@ -49,9 +50,9 @@ def loadData(dataDirectory: str = None, ratio: Union[str, float, int] = "full") 
             neg = train_neg_full
         elif ratio == 'sub':
             with open(PurePath(dataDirectory, 'train_pos.txt'), 'r', encoding='utf-8') as fp:
-                train_pos_sub = list(set(fp.readlines()))
+                train_pos_sub = fp.readlines()
             with open(PurePath(dataDirectory, 'train_neg.txt'), 'r', encoding='utf-8') as fp:
-                train_neg_sub = list(set(fp.readlines()))
+                train_neg_sub = fp.readlines()
             pos = train_pos_sub
             neg = train_neg_sub
         elif ratio == 'clean':
@@ -71,9 +72,33 @@ def loadData(dataDirectory: str = None, ratio: Union[str, float, int] = "full") 
         else:
             raise AttributeError(
                 'The input should be \'full\', \'sub\', \'clean\', or a (float) number between 0 and 1')
+    logger.debug(f"Before preprocessing: Positive: {len(pos)}, Negative: {len(neg)}, Test: {len(test_full)}")
+    pos, neg = preprocessing(pos, neg)
     logger.info(f"Dataset loaded!")
-    logger.debug(f"Positive: {len(pos)}, Negative: {len(neg)}, Test: {len(test_full)}")
+    logger.info(f"Number of sentences: Positive: {len(pos)}, Negative: {len(neg)}, Test: {len(test_full)}")
     return pos, neg, test_full
+
+
+def preprocessing(pos: list, neg: list) -> Tuple[list, list]:
+    to_be_removed = r'(<.*?>)|(\.{3})'
+
+    pos_df = pd.DataFrame({'text': pos, 'label': [1] * len(pos)})
+    pos_df['text'] = pos_df['text'].str.strip().replace(to_be_removed, '')
+    pos_count = pos_df.groupby(['text']).count()
+
+    neg_df = pd.DataFrame({'text': neg, 'label': [-1] * len(neg)})
+    neg_df['text'] = neg_df['text'].str.strip().replace(to_be_removed, '')
+    neg_count = neg_df.groupby(['text']).count()
+
+    inter_df = pos_count.join(neg_count, on='text', how='inner', lsuffix='_pos', rsuffix='_neg').reset_index()
+
+    to_pos = np.select(inter_df['label_pos'] > inter_df['label_neg'], inter_df['text']).tolist()
+    to_neg = np.select(inter_df['label_pos'] < inter_df['label_neg'], inter_df['text']).tolist()
+
+    pos_result = list(filter(None, set(pos) - set(to_neg)))
+    neg_result = list(filter(None, set(neg) - set(to_pos)))
+
+    return pos_result, neg_result
 
 
 def loadDataForUnitTesting(dataDirectory: str = None, ratio: float = 0.0002) -> Tuple[list, list, list]:
