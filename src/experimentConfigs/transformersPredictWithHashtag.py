5a53bdc4bc36cc4dd9b5e848dabc793e6e55a9bb
He Liu <heliu@heliu.name>
2021-07-30 18:09:59 +0100

    document submission.py for hashtag prediction

--- src/experimentConfigs/transformersPredictWithHashtag.py
+++ src/experimentConfigs/transformersPredictWithHashtag.py
@@ -17,7 +17,7 @@ PROJECT_DIRECTORY = get_project_path()
 
 
 class TransformersPredictWithHashtag(TransformersPredict):
-    def __init__(self, freq_threshold: float = 100, prob_threshold: float = 0.6, **kwargs):
+    def __init__(self, freq_threshold: int = 500, prob_threshold: float = 0.7, **kwargs):
         super(TransformersPredictWithHashtag, self).__init__(**kwargs)
 
         self.hashtag_dict = load_hashtag_config()
